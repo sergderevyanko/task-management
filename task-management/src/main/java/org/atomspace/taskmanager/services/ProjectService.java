2c2ab842b3f1007d4891a3f93fc353149c6afd77
Sergey Derevyanko <sergey.derevyanko@opower.com>
2019-07-30 14:19:31 +0300

    Add delete by ID logic

--- task-management/src/main/java/org/atomspace/taskmanager/services/ProjectService.java
+++ task-management/src/main/java/org/atomspace/taskmanager/services/ProjectService.java
@@ -37,4 +37,14 @@ public class ProjectService {
         return projectRepository.findAll();
     }
 
+    public void deleteProjectByIdentifier(String projectId){
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+        if(project == null){
+            throw new ProjectIdException("Cannot delete Project with ID '" + projectId + "'. " +
+                    "Project does not exist");
+        }
+
+        projectRepository.delete(project);
+    }
+
 }
