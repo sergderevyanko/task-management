d490b5b98720777fc2525abc171054f36b3553e7
Sergey Derevyanko <sergey.derevyanko@opower.com>
2019-07-30 10:42:57 +0300

    Add a basic validation

--- task-management/src/main/java/org/atomspace/taskmanager/web/ProjectController.java
+++ task-management/src/main/java/org/atomspace/taskmanager/web/ProjectController.java
@@ -5,10 +5,13 @@ import org.atomspace.taskmanager.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 /**
  * Created by sergey.derevyanko on 30.07.19.
@@ -20,7 +23,12 @@ public class ProjectController {
     private ProjectService projectService;
 
     @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project){
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project,
+                                                    BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            return new ResponseEntity<String>("Invalid Project Object", HttpStatus.BAD_REQUEST);
+        }
+
         Project createdProject = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(createdProject, HttpStatus.CREATED);
     }
