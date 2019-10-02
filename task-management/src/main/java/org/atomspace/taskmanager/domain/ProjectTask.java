08340a6b91a4a6d90ef47da01c1845b6aa462b67
Sergey Derevyanko <sergey.derevyanko@opower.com>
2019-10-02 10:36:51 +0300

    S4L47: Add Backlog - ProjectTask relationship

--- task-management/src/main/java/org/atomspace/taskmanager/domain/ProjectTask.java
+++ task-management/src/main/java/org/atomspace/taskmanager/domain/ProjectTask.java
@@ -1,5 +1,7 @@
 package org.atomspace.taskmanager.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import java.util.Date;
@@ -17,7 +19,13 @@ public class ProjectTask {
     private String acceptanceCriteria;
     private Integer priority;
     private Date dueDate;
+
     //ManyToOne with Backlog
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.REFRESH)
+    @JoinColumn(name = "backlog_id", updatable = false, nullable = false)
+    @JsonIgnore
+    private Backlog backlog;
+
     @Column(updatable = false)
     private String projectIdentifier;
 
@@ -107,6 +115,14 @@ public class ProjectTask {
     @PreUpdate
     protected void onUpdate(){
         this.updatedAt = new Date();
+    }
+
+    public Backlog getBacklog() {
+        return backlog;
+    }
+
+    public void setBacklog(Backlog backlog) {
+        this.backlog = backlog;
     }
 
     @Override
