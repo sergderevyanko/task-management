6ae1f7d24444b86a4505ac4c38087b35efc15b44
Sergey Derevyanko <sergey.derevyanko@opower.com>
2019-10-09 18:46:14 +0300

    S5L67: Finished Add projectTask, handle errors, setup Backlog and ProjectTask component

--- task-mngmt-ui/src/actions/backlogActions.js
+++ task-mngmt-ui/src/actions/backlogActions.js
@@ -1,7 +1,19 @@
 import axios from "axios";
+import {GET_ERRORS} from "./types";
 
 
 export const addProjectTask  = (project_code, project_task, history)=> async dispatch =>{
-    await axios.post(`/api/backlog/${project_code}`, project_task);
-    history.push(`/projectBoard/${project_code}`);
+    try{
+        await axios.post(`/api/backlog/${project_code}`, project_task);
+        history.push(`/projectBoard/${project_code}`);
+        dispatch({
+            type: GET_ERRORS,
+            payload: {}
+        });
+    } catch (err) {
+        dispatch({
+            type: GET_ERRORS,
+            payload: err.response.data
+        });
+    }
 }
--- task-mngmt-ui/src/components/ProjectBoard/ProjectTasks/AddProjectTask.js
+++ task-mngmt-ui/src/components/ProjectBoard/ProjectTasks/AddProjectTask.js
@@ -24,8 +24,15 @@ class AddProjectTask extends Component {
 
     }
 
+    componentWillReceiveProps(nextProps) {
+        if(nextProps.errors) {
+            this.setState({errors: nextProps.errors});
+        }
+    }
+
     render() {
         const {id} = this.props.match.params;
+        const {errors} = this.state;
         return (
             <div className="add-PBI">
                 <div className="container">
@@ -38,12 +45,18 @@ class AddProjectTask extends Component {
                             <p className="lead text-center">Project Name + Project Code</p>
                             <form onSubmit={this.onSubmit}>
                                 <div className="form-group">
-                                    <input type="text" className="form-control form-control-lg"
+                                    <input type="text"
+                                           className={classnames("form-control form-control-lg", {"is-invalid": errors.summary})}
                                            name="summary"
                                            placeholder="Project Task summary"
                                            value={this.state.summary}
                                            onChange={this.onChange}
                                     />
+                                    {errors.summary && (
+                                        <div className="invalid-feedback">
+                                            {errors.summary}
+                                        </div>
+                                    )}
                                 </div>
                                 <div className="form-group">
                                     <textarea className="form-control form-control-lg" placeholder="Acceptance Criteria"
@@ -109,7 +122,12 @@ class AddProjectTask extends Component {
 }
 
 AddProjectTask.propTypes = {
-    addProjectTask: PropTypes.func.isRequired
+    addProjectTask: PropTypes.func.isRequired,
+    errors: PropTypes.object.isRequired
 }
 
-export default connect(null, {addProjectTask})(AddProjectTask);
+const mapStateToProps = state=>({
+    errors: state.errors
+});
+
+export default connect(mapStateToProps, {addProjectTask})(AddProjectTask);
