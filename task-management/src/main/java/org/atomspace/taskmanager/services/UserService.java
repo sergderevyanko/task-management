99de129a3741b7b76b7b93ec9cd622aaa9d41a88
Sergey Derevyanko <sergey.derevyanko@opower.com>
2019-10-17 10:09:30 +0300

    S5L80: Create User, UserRepository, UserService

---
+++ task-management/src/main/java/org/atomspace/taskmanager/services/UserService.java
@@ -0,0 +1,13 @@
+package org.atomspace.taskmanager.services;
+
+import org.atomspace.taskmanager.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+}
