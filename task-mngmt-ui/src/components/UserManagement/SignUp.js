acd1e2b953d8297e7c2051082e07952fad55e98c
Sergey Derevyanko <sergey.derevyanko@opower.com>
2019-10-22 16:19:18 +0300

    S7L93: User registration - happy path

--- task-mngmt-ui/src/components/UserManagement/SignUp.js
+++ task-mngmt-ui/src/components/UserManagement/SignUp.js
@@ -1,6 +1,42 @@
 import React, {Component} from 'react';
+import {createNewUser} from "../../actions/securityActions";
+import PropTypes from "prop-types";
+import classnames from "classnames";
+import {connect} from "react-redux";
 
 class SignUp extends Component {
+
+    constructor(){
+        super();
+        this.state = {
+            username: "",
+            fullName: "",
+            password: "",
+            confirmPassword: "",
+            errors: {}
+        };
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    onChange(e){
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    onSubmit(e){
+        e.preventDefault();
+        const newUser = {
+            username: this.state.username,
+            fullName: this.state.fullName,
+            password: this.state.password,
+            confirmPassword: this.state.confirmPassword
+        }
+        this.props.createNewUser(newUser, this.props.history);
+    }
+
+
     render() {
         return (
             <div className="register">
@@ -9,25 +45,33 @@ class SignUp extends Component {
                         <div className="col-md-8 m-auto">
                             <h1 className="display-4 text-center">Sign Up</h1>
                             <p className="lead text-center">Create your Account</p>
-                            <form action="create-profile.html">
+                            <form action="" onSubmit={this.onSubmit}>
                                 <div className="form-group">
-                                    <input type="text" className="form-control form-control-lg" placeholder="Name"
-                                           name="name"
+                                    <input type="text" className="form-control form-control-lg" placeholder="Full Name"
+                                           name="fullName"
+                                           value={this.state.fullName}
+                                           onChange={this.onChange}
                                            required/>
                                 </div>
                                 <div className="form-group">
                                     <input type="email" className="form-control form-control-lg"
-                                           placeholder="Email Address" name="email"/>
+                                           placeholder="Username (Email)" name="username"
+                                           value={this.state.username}
+                                    onChange={this.onChange}/>
 
                                 </div>
                                 <div className="form-group">
                                     <input type="password" className="form-control form-control-lg"
-                                           placeholder="Password" name="password"/>
+                                           placeholder="Password" name="password"
+                                           value={this.state.password}
+                                           onChange={this.onChange}
+                                    />
                                 </div>
                                 <div className="form-group">
                                     <input type="password" className="form-control form-control-lg"
                                            placeholder="Confirm Password"
-                                           name="password2"/>
+                                           name="confirmPassword" value={this.state.confirmPassword}
+                                           onChange={this.onChange}/>
                                 </div>
                                 <input type="submit" className="btn btn-info btn-block mt-4"/>
                             </form>
@@ -39,4 +83,13 @@ class SignUp extends Component {
     }
 }
 
-export default SignUp;
+SignUp.propTypes = {
+    createNewUser: PropTypes.func.isRequired,
+    errors: PropTypes.object.isRequired
+}
+
+const mapStateToProps = state => {
+    errors: state.errors
+}
+
+export default connect(mapStateToProps, {createNewUser})(SignUp);
