3d3196b689314dbdce97a6c5a79e06e2348dac38
Sergey Derevyanko <sergey.derevyanko@opower.com>
2019-10-27 08:21:19 +0200

    S7L102: Bug Fixes: Enable to run on Chrome without react dev tools installed

--- task-mngmt-ui/src/store.js
+++ task-mngmt-ui/src/store.js
@@ -7,17 +7,21 @@ const middleware = [thunk]
 
 let store;
 
-if(window.navigator.userAgent.includes("Chrome")){
+//Make it working without React/Redux tools installed
+const ReactReduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+
+if(window.navigator.userAgent.includes("Chrome") && ReactReduxDevTools){
     store = createStore(
         rootReducer,
         initialState,
         compose(applyMiddleware(...middleware),
         // from https://github.com/zalmoxisus/redux-devtools-extension
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+            ReactReduxDevTools
+        )
     );
 }
 else {
-    // don't use chrone extension in non-chrome
+    // don't use chrome extension in non-chrome
     store = createStore(
         rootReducer,
         initialState,
